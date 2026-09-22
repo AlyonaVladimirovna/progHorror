71b41a047fef64887d3abda79dfa6fc30ed5713c
Alyona Babinceva <seinei0443@gmail.com>
2026-09-22 11:23:05 +0600

    Созданы списки констант для меню и инвентаря, сделана функция вывода меню

--- main.c
+++ main.c
@@ -1,8 +1,49 @@
 //Лаба 2, Вариант 6
 #include <stdio.h>
+#include <locale.h>
+
+
+enum MENU {
+	MENU_exit = 0,
+	MENU_clock = 1,
+	MENU_work = 2,
+	MENU_inventory = 3,
+	MENU_put = 4,
+	MENU_trow = 5,
+	MENU_weight = 6
+};
+
+enum INVENTORY {
+	INVENTORY_empty = 0,
+	INVENTORY_wood = 1,
+	INVENTORY_stone = 2,
+	INVENTORY_seed = 3,
+	INVENTORY_water = 4,
+	INVENTORY_flower = 5,
+	INVENTORY_potato = 6,
+	INVENTORY_wheat = 7, 
+	INVENTORY_showel = 8,
+	INVENTORY_axe = 9
+};
+
+int menuSelect() {
+
+};
+
+void menuPrint() {
+	printf("[%d] Выход \n[%d] Посмотреть на часы\n[%d] Поработать\n[%d] Посмотреть инвентарь\n[%d] Положить в инвентарь\n[%d] Выбросить из инвентаря\n[%d] Поиск тяжестей", \
+		MENU_exit, MENU_clock, MENU_work, MENU_inventory, MENU_put, MENU_trow, MENU_weight);
+};
 
 int main() {
-	int current_day, current_hour, inventory[10];
+	setlocale(LC_ALL, ".UTF-8");
+	int current_day = 1, current_hour = 8;
+
+	int inventory[10] = { INVENTORY_empty, INVENTORY_empty, INVENTORY_axe, INVENTORY_wood, INVENTORY_wood, \
+						INVENTORY_flower, INVENTORY_empty, INVENTORY_seed, INVENTORY_empty, INVENTORY_wood };
+
+	menuPrint();
 
 
-}
+
+};
